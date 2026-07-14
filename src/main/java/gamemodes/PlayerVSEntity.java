8657ad6a85752efabdf088b3e9867f78c19549be
CodeAFox <150443592+CodeAFox@users.noreply.github.com>
2026-07-14 23:05:31 +0200

    System choice is now outputted

--- src/main/java/gamemodes/PlayerVSEntity.java
+++ src/main/java/gamemodes/PlayerVSEntity.java
@@ -30,6 +30,8 @@ public class PlayerVSEntity implements GameMode
 
   public PlayedObject randomChoice()
   {
-    return choices.get(rand.nextInt(choices.size()));
+    PlayedObject systemChoice = choices.get(rand.nextInt(choices.size()));
+    System.out.println("The computer played: " + systemChoice);
+    return systemChoice;
   }
 }
